918ccd5dea91824e7da810651013b9d131e77ca5
penta-dev-1 <m.soliman@penta-b.com>
2021-02-05 21:55:05 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 ecs-cli configure profile --profile-name $profile --access-key $accesskey --secret-key $secretkey
 ecs-cli configure --cluster $cluster_name --default-launch-type $launch_type --region $region --config-name $config_name 
-ecs-cli compose up  --cluster-config $config_name --ecs-profile $profile
+ecs-cli compose up  --cluster-config $config_name --ecs-profile $profile --project-name front
 
 
